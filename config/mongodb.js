d8a68c8b6f46312a78f3e7d3bb4c68a1935083b8
Amaljith A A <amaljith829@gmail.com>
2025-05-27 19:03:07 +0530

    db

--- config/mongodb.js
+++ config/mongodb.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 mongoose.connection.on("connected", () => console.log("MongoDB connected successfully"))
 
 const connectDB = async () => { 
-    await mongoose.connect(`${process.env.MONGO_URI}/WellCare`)
+    await mongoose.connect(`${process.env.MONGODB_URI}/WellCare`)
 
 }
 
